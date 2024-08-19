fa4607242debf3ab08824ac23af21d91b2aced6b
Farhan-prottoy <86864852+Farhan-prottoy@users.noreply.github.com>
2024-08-19 19:30:41 +0600

    2nd commit
    Added songs and some other parts
    more to come

---
+++ script.js
@@ -0,0 +1,66 @@
+// async function main() {
+//   let a = await fetch("http://127.0.0.1:5500/songs/");
+//   let response = await a.text();
+//   console.log(response);
+//   let div = document.createElement("div");
+//   div.innerHTML = response;
+//   let as = div.getElementsByTagName("a");
+//   let songs = [];
+//   for (let index = 0; index < as.length; index++) {
+//     const element = as[index];
+//     if (element.href.endsWith(".mp3")) {
+//       songs.push(element.href);
+//     }
+//   }
+//   return songs;
+// }
+// main();
+
+async function getSongs() {
+    try {
+        // Fetch the HTML content
+        let response = await fetch("http://127.0.0.1:5500/songs/");
+        let text = await response.text();
+
+        // Create a new DOMParser to parse the HTML
+        let parser = new DOMParser();
+        let doc = parser.parseFromString(text, "text/html");
+
+        // Get all <a> elements
+        let as = doc.getElementsByTagName("a");
+        let songs = [];
+
+        // Filter <a> elements for .mp3 links
+        for (let index = 0; index < as.length; index++) {
+            const element = as[index];
+            if (element.href.endsWith(".mp3")) {
+                songs.push(element.href.split("/songs/")[1]);
+            }
+        }
+
+        // Output or return the list of songs
+        console.log(songs);
+        return songs;
+    } catch (error) {
+        console.error("Error fetching or processing data:", error);
+    }
+}
+async function main() {
+    
+    let songs = await getSongs();
+    console.log(songs);
+
+      let songUL = document.querySelector(".songList").getElementsByTagName("ul")[0]
+    for (const song of songs) {
+        songUL.innerHTML = songUL.innerHTML + `<li>${song.replaceAll("%20", " ")}</li>`;
+    }
+
+
+    var audio = new Audio(songs[0]);
+    audio.play();
+
+    audio.addEventListener("loadeddata", () =>{
+        console.log(audio.duration, audio.currentSrc,audio.currentTime);
+    });
+}
+main();
